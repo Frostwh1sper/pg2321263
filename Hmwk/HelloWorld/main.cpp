ed542f90172244bdd58a02f29390715229c10be4
Frostwh1sper <patrick.glenning@gmail.com>
2014-06-24 17:29:49 -0700

    Updating homework & project folders

---
+++ Hmwk/HelloWorld/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Patrick
+ * Created on June 24, 2014, 12:25 AM
+ * Purpose: Testing NetBeans Installation
+ */
+
+//System Library 
+#include <iostream>
+using namespace std;
+
+//Program Exucution Starts Here
+int main(int argc, char** argv) {
+    
+    //Display Simple Text
+    cout<<"Hello world!"<<endl;
+
+    //Finish him!!
+    return 0;
+}
+
