e613cfb9ee13cc312c6a5f1cae2a3ecd8aaa7ac6
Frostwh1sper <patrick.glenning@gmail.com>
2014-07-07 11:56:47 -0700

    Signed-off-by: Frostwh1sper <patrick.glenning@gmail.com>

--- Hmwk/Gaddis_6thEd_Chap4_Prob22/main.cpp
+++ Hmwk/Gaddis_6thEd_Chap4_Prob22/main.cpp
@@ -22,7 +22,8 @@ int main(int argc, char** argv) {
 
     //Declare and initialize variables
     float rate1=0.12, rate2=0.55, rate3=0.35;   //Rates from 0000-0659, 0700-1900, 1901-2359
-    int min, time;
+    int min;
+    float time;
     bool loop=true;
     
     //User input
@@ -30,34 +31,29 @@ int main(int argc, char** argv) {
     cout << "Please enter the duration of your phone call, in minutes: ";
     cin >> min;
     do{
-        cout << "What time was the call made?" << endl;
-        cout << "1) Midnight - 6:59AM" << endl;
-        cout << "2) 7AM - 7PM" << endl;
-        cout << "3) 7PM and 11:59PM" << endl;
+        cout << "What time was the call made? (Format HH.MM)" << endl;
         cin >> time;
     
     //Output
         cout << setprecision(2) << fixed;
-        switch(time){
-            case 1:{
-                cout << "Your charges, for the call lasting " << min << " minutes, is $" << min*rate1 << endl;
-                loop=false;
-                break;
+        if(time>=00.00 && time<=06.59){
+            cout << "Your charges, for the call lasting " << min << " minutes, is $" << min*rate1 << endl;
+            loop=false;
+            break;
             }
-            case 2:{
-                cout << "Your charges, for the call lasting " << min << " minutes, is $" << min*rate2 << endl;
-                loop=false;
-                break;
+        else if(time>=07.00 && time<=19.00){
+            cout << "Your charges, for the call lasting " << min << " minutes, is $" << min*rate2 << endl;
+            loop=false;
+            break;
             }
-            case 3:{
-                cout << "Your charges, for the call lasting " << min << " minutes, is $" << min*rate3 << endl;
-                loop=false;
-                break;
+        else if(time>=19.01 && time<=23.59){
+            cout << "Your charges, for the call lasting " << min << " minutes, is $" << min*rate3 << endl;
+            loop=false;
+            break;
             }
-            default:{
-                cout << "You did not select an appropriate time for the start of the phone call, please try again." << endl;
+        else{
+            cout << "You did not select an appropriate time for the start of the phone call, please try again." << endl;
             }
-    }
     }while(loop);
     
     //Finish him!!
