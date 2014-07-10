b2c9f582607049413da574e98e26054347f272a0
Frostwh1sper <patrick.glenning@gmail.com>
2014-07-09 17:00:26 -0700

    Project 1

---
+++ Projects/Project 1/Mastermind/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Patrick Glenning
+ * Created on July 9, 2014, 4:56 PM
+ * Purpose: Execute the game Mastermind
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Begin execution
+int main(int argc, char** argv) {
+    
+    //Seed RNG
+    unsigned seed=time(0);
+    srand(seed);
+    
+    //Declare and initialize variables
+    
+    //Randomly generate the 'code'
+    
+
+    //Finish him!!
+    return 0;
+}
+
