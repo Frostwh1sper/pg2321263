69c507844f66b1ba7d2496d27c0876de3dbadc11
Frostwh1sper <patrick.glenning@gmail.com>
2014-07-09 17:15:19 -0700

    Project 1

--- Projects/Project 1/Mastermind/main.cpp
+++ Projects/Project 1/Mastermind/main.cpp
@@ -26,8 +26,46 @@ int main(int argc, char** argv) {
     srand(seed);
     
     //Declare and initialize variables
+    char color[4];  //Array of four colors chosen for the code
     
-    //Randomly generate the 'code'
+    //Randomly generate the 'code' to be cracked
+    for(int i=0; i<4; i++){
+        int colNum=rand()%6+1; //Switch operator for assigning color numbers
+        switch(colNum){
+            case 1:{
+                color[i]='R';
+                break;
+            }
+            case 2:{
+                color[i]='O';
+                break;
+            }
+            case 3:{
+                color[i]='Y';
+                break;
+            }
+            case 4:{
+                color[i]='G';
+                break;
+            }
+            case 5:{
+                color[i]='B';
+                break;
+            }
+            case 6:{
+                color[i]='P';
+                break;
+            }
+        }
+    }
+    
+    //Code output test (comment block when running finalized game debug)
+    for(int i=0; i<4; i++){
+        cout << color[i] << " ";
+    }
+    cout << endl;
+    
+    //User input
     
 
     //Finish him!!
